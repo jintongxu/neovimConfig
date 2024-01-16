15eb07784a51bf2e994de8abd0bc727020a5e710
jintongxu <tofuture0802@gmail.com>
2024-01-16 09:31:17 +0000

    增加一下键位映射

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "gp", "<c-t>")
+vim.keymap.set("n", "gp", "<c-t>", { desc = "ctrl-t" })
+vim.keymap.set("i", "jk", "<ESC>")
+vim.keymap.set("n", "H", "^")
+vim.keymap.set("n", "L", "$")
+vim.keymap.set("n", "<leader>ms", "<cmd>w<cr>", { desc = "save file" })
