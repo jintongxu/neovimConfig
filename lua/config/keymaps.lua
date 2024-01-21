958f2b736ce88782d9c4672d82efa8c915d77c11
jintongxu <simon_yolo@163.com>
2024-01-21 22:20:21 +0800

    增加fcitx插件----如果用的fcitx输入法,vim进入normal模式自动切换到英文输入法

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "gp", "<c-t>", { desc = "ctrl-t" })
-vim.keymap.set("i", "jk", "<ESC>")
 vim.keymap.set("n", "H", "^")
 vim.keymap.set("n", "L", "$")
 vim.keymap.set("n", "<leader>ms", "<cmd>w<cr>", { desc = "save file" })
---
+++ lua/plugins/fcitx.lua
@@ -0,0 +1,5 @@
+return {
+  {
+    "h-hg/fcitx.nvim",
+  },
+}
